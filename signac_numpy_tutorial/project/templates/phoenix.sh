3a36b1383cffb974209fc941de149cd3687266af
Brad Crawford <bcrawford@gatech.edu>
2024-04-18 16:04:55 -0400

    added input checks and mpi using all GPUs if CPUs>GPUs

--- signac_numpy_tutorial/project/templates/phoenix.sh
+++ signac_numpy_tutorial/project/templates/phoenix.sh
@@ -3,8 +3,8 @@
 {% block header %}
 {% set np_max = operations|map(attribute='directives.np')|max %}
 {% set mem_per_cpu = operations|map(attribute='directives.mem-per-cpu')|max %}
-{% set cpus_per_per_task = operations|map(attribute='directives.cpus-per-task')|max  %}
-{% set gpus_per_per_task = operations|map(attribute='directives.gpus-per-task')|max  %}
+{% set cpus_per_task = operations|map(attribute='directives.cpus-per-task')|max  %}
+{% set gpus_per_task = operations|map(attribute='directives.gpus-per-task')|max  %}
 
     {{- super () -}}
 
@@ -14,18 +14,17 @@
 #SBATCH --error=/dev/null
 #SBATCH -N 1
 #SBATCH --ntasks-per-node={{ np_global }}
+#SBATCH --cpus-per-task={{ cpus_per_task }}
 #SBATCH --mem-per-cpu={{ mem_per_cpu }}G
 
-{% if gpus_per_per_task == 0 %}
+{% if gpus_per_task == 0 %}
 #SBATCH -p cpu-small
 {%- endif %}
 
-#SBATCH --cpus-per-task={{ cpus_per_per_task }}
-
-{% if gpus_per_per_task > 0 %}
+{% if gpus_per_task > 0 %}
 #SBATCH -p gpu-a100
-#SBATCH --gres gpu:{{ np_global * gpus_per_per_task }}
-#SBATCH --gpus-per-task={{ gpus_per_per_task }}
+#SBATCH --gres gpu:{{ np_global * gpus_per_task }}
+#SBATCH --gpus-per-task={{ gpus_per_task }}
 {%- endif %}
 
 
@@ -36,7 +35,7 @@ echo  "Time is" date
 module load anaconda3
 
 # Add any modules here needed only for the GPU versions
-{% if gpus_per_per_task %}
+{% if gpus_per_task %}
 module load cuda/12.1.1-6oacj6
 {%- endif %}
 
