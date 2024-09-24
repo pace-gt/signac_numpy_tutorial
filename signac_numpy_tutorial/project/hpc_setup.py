76504a90e2fb0fb436ea24bd42c3c219af4838d2
bcrawford39GT <130703978+bcrawford39GT@users.noreply.github.com>
2024-09-24 15:56:42 -0400

    Update hpc_setup.py to RHEL 9

--- signac_numpy_tutorial/project/hpc_setup.py
+++ signac_numpy_tutorial/project/hpc_setup.py
@@ -7,6 +7,6 @@ class Phoenix(DefaultSlurmEnvironment):
     """Subclass of DefaultSlurmEnvironment for GT Phoenix HPC."""
     
     # Find the hostname by loggin in the HPC and using the 'hostname' command.
-    # In this case 'hostname' produced 'login-phoenix-slurm-2.pace.gatech.edu'.
-    hostname_pattern = r"login-phoenix-slurm-.\.pace\.gatech\.edu"
+    # In this case 'hostname' produced 'login-phoenix-rh9--2.pace.gatech.edu'.
+    hostname_pattern = r"login-phoenix-rh9-.\.pace\.gatech\.edu"
     template = "phoenix.sh"
